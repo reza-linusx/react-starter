bb4a022190a0c1f126cb516d9c3d30315f35becc
Reza Br <reza.linusx@gmail.com>
2024-08-07 05:51:39 +0330

    React basics - JSX - Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// this a react component
+// a react component must start with capital letter
+// because html tags are lower case by default
+function MyButton() {
+  return (
+    // this is not html - but it is called jsx
+    // your components should only return one element or <>...</> wrapper
+    // In React, you specify a CSS class with className
+    <button className='my-button'>I am a button</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello from react</h1>
+      {/* here is how you can use the component */}
+      <MyButton/>
+      <MyButton/>
     </div>
   );
 }
 
+// this specifies the the main component in the file
 export default App;
